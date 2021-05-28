48e666b9f95abe3345b41a0d4319c666364c3e9d
Sinan <sinan@sinan-mbp.local>
2021-05-28 21:53:18 +0300

    Create project

---
+++ WatchConnectivity-WatchKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchConnectivity-WatchKit
+//
+//  Created by Sinan on 28.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
