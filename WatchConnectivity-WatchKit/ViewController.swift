18fcf4de48cc7ad10ddd0b45305cf2a8ff10d83c
Sinan <sinan@sinan-mbp.local>
2021-05-28 22:09:03 +0300

    Configure ViewController

--- WatchConnectivity-WatchKit/ViewController.swift
+++ WatchConnectivity-WatchKit/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var detailsTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        if taskTextField.text != ""{
+            WatchManager.shared.sendDataToWatch(data: ["task" : taskTextField.text!, "details" : detailsTextField.text ?? ""])
+        }
+    }
 }
 
